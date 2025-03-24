623209f4dca119e5ced5b1a23c1576b27e2caa8d
Sanjaratianaa <119104188+Sanjaratianaa@users.noreply.github.com>
2025-03-24 22:45:46 +0300

    fix deploy 1.0.4

--- src/environnements/environnement.prod.ts
+++ src/environnements/environnement.prod.ts
@@ -1,5 +1,6 @@
 export const environment = {
     production: true,
-    apiUrl: process.env['APP_BASE_URL']
+    // apiUrl: process.env['APP_BASE_URL']
+    apiUrl: "https://m1-garage-back-end.onrender.com/api"
   };
   
