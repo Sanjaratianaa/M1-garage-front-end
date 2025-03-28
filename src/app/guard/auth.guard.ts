72babea6a403a89f6d9712e50a9a9e25dbe185d7
Rota <rota.dev@et.in>
2025-03-28 17:31:41 +0300

    gestion du token

--- src/app/guard/auth.guard.ts
+++ src/app/guard/auth.guard.ts
@@ -17,15 +17,24 @@ export class AuthGuard implements CanActivateChild {
     const token = localStorage.getItem('token');
     const user = JSON.parse(localStorage.getItem('user') || '{}');
 
+    // console.log("state.url: " + state.url);
+
+    // Si la page actuelle est une page de login, ne pas bloquer l'accès
+    const loginPages = ['/authentication/login-client', '/authentication/login-manager', '/authentication/login-mecanicien', '/login'];
+    if (loginPages.includes(state.url)) {
+      // console.log("ouiiiii loginnnnn")
+      return true;
+    }
+
     if (token && user && user.exp) {
       const currentTime = Math.floor(Date.now() / 1000); // Temps actuel en secondes
-      console.log(user.exp + " > ? " + currentTime);
+      // console.log(user.exp + " > ? " + currentTime);
       if (user.exp > currentTime) {
-        console.log('AuthGuard - Allowed Access');
+        // console.log('AuthGuard - Allowed Access');
         // Token exists, allow access to the route
         return true;
       } else {
-        console.log('AuthGuard - Redirecting to Login');
+        // console.log('AuthGuard - Redirecting to Login');
         const role = user.role.libelle;
         let redirectUrl = '/authentication/client-login';
         if (role === "manager") {
@@ -36,14 +45,16 @@ export class AuthGuard implements CanActivateChild {
 
         // return this.router.navigate([redirectUrl]).then(() => false);
         // Exécuter la redirection APRES avoir retourné false
-        // setTimeout(() => {
-        //   this.router.navigate([redirectUrl]);
-        // }, 0);
+        setTimeout(() => {
+          // console.log("redirect eooooooooooooooooooooooooo");
+          this.router.navigate([redirectUrl]);
+        }, 0);
         // document.location.href = redirectUrl;
+        // this.router.navigate([redirectUrl]);
         return false;
       }
     } else {
-      console.log('AuthGuard - Redirecting to Login');
+      // console.log('AuthGuard - Redirecting to Login');
       // No token, redirect to the login page
       this.router.navigate(['/authentication/client-login']); // Corrected redirect
       return false;
